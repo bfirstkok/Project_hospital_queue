d1d094cf73df72f5e208998d5030377882241a33
bfirstkok <bfirstkok@gmail.com>
2026-08-17 17:32:09 +0700

    Reserve page space for fixed workflow navigation

--- queues/tests.py
+++ queues/tests.py
@@ -206,6 +206,7 @@ class QueueWorkflowTests(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'class="waiting-vitals-page"')
+        self.assertContains(response, "padding-top: 62px")
         self.assertContains(response, "ดูข้อมูลผู้ป่วย")
         self.assertContains(response, f'id="patient-modal-{patient.visits.get().queue.id}"')
         self.assertContains(response, patient.phone)
