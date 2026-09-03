8591ae83b5064e4b691d4fa84059f01a0ccd4ae1
khanathippakorn Namthachak <79431068+bfirstkok@users.noreply.github.com>
2026-09-03 14:18:55 +0700

    Register nurse assignment lifecycle signals

--- queues/apps.py
+++ queues/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class QueuesConfig(AppConfig):
-    name = 'queues'
+    name = "queues"
+
+    def ready(self):
+        # Register lifecycle hooks that release nurse capacity when a visit ends.
+        from . import signals  # noqa: F401
