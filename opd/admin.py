007fe08f93c0914cca308c84d85ff0405af47e61
khanathippakorn Namthachak <79431068+bfirstkok@users.noreply.github.com>
2026-09-18 13:14:08 +0700

    Register OPD assessment in admin

--- opd/admin.py
+++ opd/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import VisitAssessment
+
+
+@admin.register(VisitAssessment)
+class VisitAssessmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "visit",
+        "examiner",
+        "opd_urgency",
+        "next_appointment_at",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("opd_urgency", "created_at")
+    search_fields = (
+        "visit__patient__hn",
+        "visit__patient__national_id",
+        "visit__patient__first_name",
+        "visit__patient__last_name",
+        "examiner__username",
+        "examiner__first_name",
+        "examiner__last_name",
+        "chief_complaint",
+        "diagnosis",
+        "treatment",
+    )
+    readonly_fields = ("created_at", "updated_at", "opd_urgency", "opd_reason")
+    date_hierarchy = "created_at"
