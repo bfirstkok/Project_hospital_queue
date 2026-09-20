5c49db4ee80596f4565d6f5859698be95c0a232e
khanathippakorn Namthachak <79431068+bfirstkok@users.noreply.github.com>
2026-09-20 16:21:41 +0700

    Enable superuser role simulation middleware

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'accounts.middleware.SuperuserRoleSimulationMiddleware',
     'queues.middleware.StaffActivityMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
